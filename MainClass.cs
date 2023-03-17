62672e2f8811aa3870e3f70719ce9d5ca0537f3d
Andrew <86121252+SlowsieNT@users.noreply.github.com>
2023-03-17 01:01:58 +0000

    Initial commit

---
+++ MainClass.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace trayer
+{
+    public class MainClass
+    {
+        string appName = "";
+        NotifyIcon TrayIcon = new NotifyIcon();
+        ContextMenuStrip Menu1 = new ContextMenuStrip();
+        Dictionary<IntPtr, object> Handles = new Dictionary<IntPtr, object>();
+        IntPtr Handle = IntPtr.Zero;
+        public MainClass(string[] args)
+        {
+            Handle = Process.GetCurrentProcess().Handle;
+            appName = Application.ProductName;
+            // Show tray icon
+            TrayIcon.Text = appName;
+            TrayIcon.Icon = Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location);
+            TrayIcon.Visible = true;
+            TrayIcon.ContextMenuStrip = Menu1;
+            // Add context menu
+            var optWindow = (ToolStripMenuItem)Menu1.Items.Add("Window");
+            var optStartup = (ToolStripMenuItem)Menu1.Items.Add("Startup");
+            var optExit = (ToolStripMenuItem)Menu1.Items.Add("Exit");
+            optStartup.Checked = ToggleStartup(true, true);
+            optStartup.Click += (s, e) => {
+                optStartup.Checked = !optStartup.Checked;
+                ToggleStartup(optStartup.Checked);
+            };
+            // Add timer
+            var tmr = new System.Windows.Forms.Timer();
+            tmr.Interval = 100;
+            tmr.Tick += (s, e) => {
+                var wHandle = Native32.GetActiveWindow();
+                if (wHandle == Handle) return;
+                var wInfo = Native32.GetActiveWindowInfo(wHandle, true);
+                var wProc = wInfo[2] as Process;
+                if ("explorer" == wProc.ProcessName && "Program Manager" == ""+wInfo[1])
+                    return;
+                if (Control.MouseButtons != MouseButtons.Middle)
+                    return;
+                if (Handles.ContainsKey(wHandle))
+                    return;
+                Handles.Add(wHandle, wInfo);
+                var wText = "" + wInfo[1];
+                var wIcon = wInfo[0] as Icon;
+                string wTitle = wText;
+                if (wText.Length > 16) wTitle = wTitle.Substring(0, 16) + "…"; 
+                var optSubWindow = (ToolStripMenuItem)optWindow.DropDownItems.Add(wTitle);
+                try { optSubWindow.Image = wIcon.ToBitmap(); } catch { }
+                optSubWindow.Click += (s2, e2) => {
+                    Handles.Remove(wHandle);
+                    Native32.ShowActiveWindow(wHandle, true);
+                    optWindow.DropDownItems.Remove(optSubWindow);
+                };
+                Native32.ShowActiveWindow(wHandle, false);
+            };
+            tmr.Enabled = true;
+        }
+        bool ToggleStartup(bool aAdd, bool aCheck=false)
+        {
+            // etc...
+            var regName = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
+            var key = Registry.CurrentUser.OpenSubKey(regName, true);
+            if (aCheck)
+                return null != key.GetValue(appName, null);
+            if (aAdd)
+                key.SetValue(appName, Application.ExecutablePath);
+            else
+                key.DeleteValue(appName, false);
+            return !aAdd;
+        }
+    }
+}
---
+++ native32.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace trayer
+{
+    internal class Native32
+    {
+        [DllImport("user32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Ansi, EntryPoint = "GetWindowTextLengthA", ExactSpelling = true, SetLastError = true)]
+        private static extern int GetWindowTextLength(IntPtr hwnd);
+
+        [DllImport("user32", CharSet = CharSet.Ansi, EntryPoint = "GetWindowTextA", ExactSpelling = true, SetLastError = true)]
+        private static extern int GetWindowText(IntPtr hwnd, [MarshalAs(UnmanagedType.VBByRefStr)] ref string lpString, int cch);
+
+        [DllImport("user32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        private static extern int ShowWindow(IntPtr hwnd, int nCmdShow);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out int lpdwProcessId);
+
+        [DllImport("user32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        public static extern short GetAsyncKeyState(int vKey);
+        public static IntPtr GetActiveWindow() { return GetForegroundWindow(); }
+        public static string GetActiveWindowTitle() => GetActiveWindowTitle(IntPtr.Zero);
+        public static Process GetActiveWindowProcess(IntPtr hWnd)
+        {
+            if (IntPtr.Zero == hWnd)
+                hWnd = GetForegroundWindow();
+            GetWindowThreadProcessId(hWnd, out int lpdwProcessId);
+            var p = Process.GetProcessById(lpdwProcessId);
+            return p;
+        }
+        public static string GetActiveWindowTitle(IntPtr hWnd)
+        {
+            if (IntPtr.Zero == hWnd)
+                hWnd = GetForegroundWindow();
+            int len = GetWindowTextLength(hWnd);
+            if (1 > len++) return "";
+            string result = "".PadLeft(len, '\0');
+            len = GetWindowText(hWnd, ref result, len);
+            return result.Substring(0, len);
+        }
+        public static Icon GetActiveWindowIcon() => GetActiveWindowIcon(IntPtr.Zero);
+        public static Icon GetActiveWindowIcon(IntPtr hWnd)
+        {
+            if (hWnd == IntPtr.Zero)
+                hWnd = GetForegroundWindow();
+            var msg = SendMessage(hWnd, 127, IntPtr.Zero, IntPtr.Zero);
+            if (IntPtr.Zero != msg)
+                return Icon.FromHandle(msg);
+            return null;
+        }
+        public static object[] GetActiveWindowInfo(IntPtr hWnd, bool aIncludeProc=false)
+        {
+            if (hWnd == IntPtr.Zero)
+                hWnd = GetForegroundWindow();
+            var wProc = aIncludeProc ? GetActiveWindowProcess(hWnd) : null;
+            var wIcon = GetActiveWindowIcon(hWnd);
+            var wText = GetActiveWindowTitle(hWnd);
+            return new object[] { wIcon, wText, wProc };
+        }
+        public static void ShowActiveWindow(IntPtr hWnd, bool aShow)
+        {
+            if (hWnd == IntPtr.Zero)
+                hWnd = GetForegroundWindow();
+            ShowWindow(hWnd, aShow ? 5 : 0);
+        }
+    }
+}
